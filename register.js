a6bfa49c974113a74d63afe150ec5ac20bf59082
tranahthi <trananhthi256@gmail.com>
2023-03-26 12:35:28 +0700

    json server

---
+++ register.js
@@ -0,0 +1,31 @@
+
+
+
+// var apiUser = ' http://localhost:3000/user';
+
+
+// function login(){
+//   getUser(handleLogin);
+
+// }
+
+// function getUser(callback){
+//   fetch(apiUser)
+//     .then(function(respone){
+//       return respone.json().then(callback);
+//     });
+// }
+
+// function handleLogin(data){
+//   var email = document.getElementById("email").value;
+//   var password = document.getElementById("password").value;
+  
+//   data.forEach(data => {
+//     if(data.email == email && data.password == password){
+//       alert("dat nhap thanh cong");
+//     }
+        
+    
+//   });
+// }
+
