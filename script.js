42622eb4e6f223300edd71a403284a9c7e6a4497
tranahthi <trananhthi256@gmail.com>
2023-03-28 10:19:11 +0700

    newupdate

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 
 
-const loginBtn = $('.login')
-console.log(loginBtn)
-const clickSearchNode = $('.click-search')
+const loginBtn = $('.login');
+console.log(loginBtn);
+// const registerBtn = $('.register');
+// console.log(registerBtn);
+const clickSearchNode = $('.click-search');
 const searchNode = $('.search-header');
 const bookRoom = $('.book-room');
+const logoHeader = $('.logo-header');
 
 
 searchNode.addEventListener('click', () => {
   clickSearchNode.style.display = 'block';
-  console.log('hehe')
+  console.log('hehe');
 })
+
+
+logoHeader.addEventListener('click', () => {
+  window.location.reload();
+  window.location = `homepage.html`;
+
+})
+
 
 // const arrowDownRoom = document.querySelector('.arrow-down-room');
 // const roomFood = document.querySelector('.room-food');
@@ -34,17 +45,13 @@ bookRoom.addEventListener('click', () => {
 })
 
 
+
+
+
 loginBtn.addEventListener('click', function login(e) {
   e.preventDefault()
   getUser(handleLogin);
 })
-
-
-
-
-
-
-
 var apiUser = ' http://localhost:3000/user';
 
 
@@ -59,17 +66,91 @@ function getUser(callback) {
 
 }
 
-function handleLogin(datas) {
+function handleLogin(users) {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  datas.forEach(data => {
-    if (data.email == email && data.password == password) {
-      alert("dat nhap thanh cong");
-      console.log('hi');
+  {
+    const user = users.find((user) => user.email === email);
+    if (user && user.password === password) {
+      localStorage.setItem("logUser", user.id);
+      localStorage.setItem("isLogin", true);
+      alert("dang nhap thanh cong");
+      window.location = `homepage.html`;
+    } else {
+      alert("Tài khoản hoặc mật khẩu không đúng");
     }
-    else {
-      alert("sai mat khau")
-    }
-  });
+  };
 }
+
+
+registerBtn.addEventListener('click', function register(e) {
+  e.preventDefault()
+  // createUser(handleRegister);
+  handleRegister();
+
+})
+
+
+function signUp(){
+  handleRegister();
+}
+
+
+function createUser (data) {
+  fetch(apiUser,{
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",  
+    },
+    body : JSON.stringify(data),
+  }).then(function(response){
+    return response.json();
+  }).catch((error) =>console.error(error));
+  if(data){
+    alert('dang ky thanh cong');
+  }
+}
+
+
+function handleRegister(){
+  
+  var firstname = document.getElementById("fisrtname").value;
+  var lastname = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+  var tel = document.getElementById("tel").value;
+  var password = document.getElementById("password").value;
+  var user = {
+    firstname : firstname,
+    lastname : lastname,
+    email : email,
+    tel : tel,
+    password : password,
+  };
+  console.log(user);
+  createUser(user);
+}
+
+
+// function handleRegister() {
+//   var registerBtn = document.querySelector('.register');
+//     registerBtn.onclick = function (){
+//       var firstname = document.querySelector('input[firstname="firstname"]').value;
+//       var lastname = document.querySelector('input[lastname="lastname"]').value;
+//       var email = document.querySelector('input[email="email"]').value;
+//       var tel = document.querySelector('input[tel="tel"]').value;
+//       var password = document.querySelector('input[password="password"]').value;
+//       var user = {
+//             firstname : firstname.value,
+//             lastname : lastname.value,
+//             email : email.value,
+//             tel : tel.value,
+//             password : password.value,
+//           };
+//           createUser(user);
+//     }
+// } 
+
+
+
+ 
 
