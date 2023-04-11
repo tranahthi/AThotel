fbacb1372418e424f47891d50605cae514533109
Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
2023-04-11 11:54:34 +0700

    .

--- bookRoom.js
+++ bookRoom.js
@@ -1,67 +1,43 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const apiBooking = 'http://localhost:3000/bookings';
+  const bookBtn = document.querySelector('.book')
+  console.log(bookBtn)
+function handleBookingRoom(e) {
+  e.preventDefault()
+  const typeRoom = document.getElementById('name-type-room').innerText;
+  const price = document.getElementById('price').innerText;
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const checkIn = document.getElementById('check-in').value;
+  const checkOut = document.getElementById('check-out').value;
+  const adult = document.getElementById('adult').value;
+  const child = document.getElementById('child').value;
+  const bookingData = {
+    name: name,
+    phone: phone,
+    checkIn: checkIn,
+    checkOut: checkOut,
+    adult: adult,
+    child: child,
+    typeRoom: typeRoom,
+    price: price,
+  };
+  createBookRoom(bookingData);
+  alert('Đặt phòng thành công');
+}
 
-const bookBtn = $('.book');
-
-
-
-
-
-
-
-
-
-
-function bookBook(){
-    handleBookingRoom ();
-  }
-  
-  bookBtn&& bookBtn.addEventListener('submit', function book(e) {
-    e.preventDefault()
-    handleBookingRoom ();
+function createBookRoom(data) {
+  fetch(apiBooking, {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(data),
   })
-  
-  
-  const apiBooking = ' http://localhost:3000/bookings';
-  
-  
-  
-  function handleBookingRoom () {
-          const typeRoom = document.getElementById('name-type-room').innerText;
-          const price = document.getElementById('price').innerText;
-          const name = document.getElementById('name').value;
-          const phone = document.getElementById('phone').value;
-          const checkIn = document.getElementById('check-in').value;
-          const checkOut= document.getElementById('check-out').value;
-          const adult = document.getElementById('adult').value;
-          const child = document.getElementById('child').value;
-          const bookingData = {
-            name : name,
-            phone : phone, 
-            checkIn : checkIn,
-            checkOut : checkOut,
-            adult : adult,
-            child : child,
-            typeRoom : typeRoom,
-            price : price,
-          };  
-          createBookRoom(bookingData);
-          alert('Đặt phòng thành công');
-  }
-  
-  function createBookRoom(data){
-    fetch(apiBooking , {
-      method: "POST",
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body : JSON.stringify(data),
-    })
-    .then(function(respone){ 
+    .then(function (respone) {
       return respone.json();
     })
     .catch(error => {
-      console.error("Đặt phòng thất bại" , error);
+      console.error("Đặt phòng thất bại", error);
     });
-  
-  }
+
+}
